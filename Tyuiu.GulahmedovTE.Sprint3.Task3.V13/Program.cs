fec5e2100bd5333387b1b9eff2a81dde2df28498
Tyuiu.GulahmedovTG@outlook.com <Tyuiu.GulahmedovTG@outlook.com>
2024-10-29 17:22:16 +0500

    Task3

---
+++ Tyuiu.GulahmedovTE.Sprint3.Task3.V13/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.GulahmedovTE.Sprint3.Task3.V13.Lib;
+namespace Tyuiu.GulahmedovTE.Sprint3.Task3.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string value = "?sd!! 5gh.s!";
+            int result = 5;
+
+            Console.WriteLine($" Искомая строка: {value}");
+            Console.WriteLine($" Искомая буква, которую нужно удалить: {result}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($" Строка в итоге: {ds.ConvertStringToInt(value)}");
+            Console.ReadKey();
+        }
+    }
+}
