6433ec9176fa2e6f8efdce9d1af6f77ad6229a6e
Tyuiu.GulahmedovTG@outlook.com <Tyuiu.GulahmedovTG@outlook.com>
2024-10-29 17:15:41 +0500

    Task2

---
+++ Tyuiu.GulahmedovTE.Sprint3.Task2.V27.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.GulahmedovTE.Sprint3.Task2.V27.Lib;
+namespace Tyuiu.GulahmedovTE.Sprint3.Task2.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 14;
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+
+            Assert.AreEqual(0.69, res);
+        }
+    }
+}
---
+++ Tyuiu.GulahmedovTE.Sprint3.Task2.V27/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.GulahmedovTE.Sprint3.Task2.V27.Lib;
+namespace Tyuiu.GulahmedovTE.Sprint3.Task2.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Начальный шаг = переменная i = 1");
+            Console.WriteLine("Конечный шаг = 14");
+            Console.WriteLine("Переменная х = 5");
+
+            DataService ds = new DataService();
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 14;
+            double sumSeries = ds.GetSumSeries(value, startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + sumSeries);
+            Console.ReadKey();
+        }
+    }
+}
