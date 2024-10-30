0e34c98df8f26d2085f8736786c80133f8617bae
Tyuiu.GulahmedovTG@outlook.com <Tyuiu.GulahmedovTG@outlook.com>
2024-10-30 16:37:18 +0500

    Task4

---
+++ Tyuiu.GulahmedovTE.Sprint3.Task4.V11/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.GulahmedovTE.Sprint3.Task4.V11.Lib;
+namespace Tyuiu.GulahmedovTE.Sprint3.Task4.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.Calculate(startValue, stopValue));
+
+            Console.ReadLine();
+        }
+    }
+}
