9da80550208990b75e98228cce91aadd140cdd14
Tyuiu.GulahmedovTG@outlook.com <Tyuiu.GulahmedovTG@outlook.com>
2024-10-30 17:01:01 +0500

    Task6

---
+++ Tyuiu.GulahmedovTE.Sprint3.Task6.V7/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.GulahmedovTE.Sprint3.Task6.V7.Lib;
+namespace Tyuiu.GulahmedovTE.Sprint3.Task6.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 17;
+            int stopValue = 26;
+
+            Console.WriteLine(" Начало отрезка = " + startValue);
+            Console.WriteLine(" Конец отрезка = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" Сумма делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
